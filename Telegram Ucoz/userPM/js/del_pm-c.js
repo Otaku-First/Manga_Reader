e4adf9427e94f00a278e17234f3cda13b52f6397
Otaku-First <otakufirstua@gmail.com>
2020-09-17 22:59:13 +0300

    otaku
    relase

--- Telegram Ucoz/userPM/js/del_pm-c.js
+++
@@ -1,7 +0,0 @@
-$('tbody tr').bind("contextmenu", function() { 
-
-var delatribute = $(this).find('td[style="background: rgb(247, 247, 247);"] a').attr('onclick');
-    $("ul.contextMenu").html('<li><a href="javascript://" onclick="'+delatribute+'"><i class="far fa-trash-alt"></i> Удалить</a></li> '); 
-	$('ul.contextMenu li a').click(function() { $("ul.contextMenu").css('display','none').fadeOut("fast"); }); event.preventDefault(); 
-	$("ul.contextMenu") .show() .css({top: event.pageY + 15, left: event.pageX + 10}); });
-     $(document).click(function() { isHovered = $("ul.contextMenu").is(":hover"); if (isHovered == false){ $("ul.contextMenu").fadeOut("fast"); } });
